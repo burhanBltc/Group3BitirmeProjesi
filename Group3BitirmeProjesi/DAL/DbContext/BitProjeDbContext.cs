560a5a0718066f1a34c8de1203a9b653a23c025d
burhanBltc <burhanbalataci@hotmail.com>
2025-01-14 13:43:07 +0300

    calisma1

--- Group3BitirmeProjesi/DAL/DbContext/BitProjeDbContext.cs
+++ Group3BitirmeProjesi/DAL/DbContext/BitProjeDbContext.cs
@@ -4,15 +4,34 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Group3BitirmeProjesi.DAL.DbContext
 {
-  
+ 
     public class BitProjeDbContext : IdentityDbContext<AppUser>
     {
-        public BitProjeDbContext(DbContextOptions<BitProjeDbContext> options)
-            : base(options) { }
-
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        public BitProjeDbContext(DbContextOptions<BitProjeDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Fluent API for Product
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+
+            // Fluent API for Category
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Products)
+                .WithOne(p => p.Category)
+                .HasForeignKey(p => p.CategoryId);
+
+            // Password validation rule (6 character minimum, 1 uppercase, 1 lowercase, 1 number, 1 special character)
+            modelBuilder.Entity<AppUser>()
+                .Property(u => u.PasswordHash)
+                .HasMaxLength(256);
+        }
     }
-
-
 }
---
+++ Group3BitirmeProjesi/DAL/Entities/Enums/Status.cs
@@ -0,0 +1,6 @@
+﻿namespace Group3BitirmeProjesi.DAL.Entities.Enums
+{
+    public interface Status
+    {
+    }
+}
